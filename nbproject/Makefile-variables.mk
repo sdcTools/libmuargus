db97698f7682e823f372d0645c573b17b4ea4f70
Peter-Paul <p.p.de.wolf@freeler.nl>
2016-08-16 11:59:27 +0200

    Config files adjusted, temp files removed

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=MinGW-Windows
 CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
-CND_ARTIFACT_NAME_Debug=libmuargusdll.dll
-CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/libmuargusdll.dll
+CND_ARTIFACT_NAME_Debug=liblibmuargus.dll
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/liblibmuargus.dll
 CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
-CND_PACKAGE_NAME_Debug=libmuargusdll.dll.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/libmuargusdll.dll.tar
+CND_PACKAGE_NAME_Debug=liblibmuargus.dll.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/liblibmuargus.dll.tar
 # Release configuration
 CND_PLATFORM_Release=MinGW-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
-CND_ARTIFACT_NAME_Release=libmuargusdll.dll
-CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/libmuargusdll.dll
+CND_ARTIFACT_NAME_Release=liblibmuargus.dll
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/liblibmuargus.dll
 CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
-CND_PACKAGE_NAME_Release=libmuargusdll.dll.tar
-CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/libmuargusdll.dll.tar
+CND_PACKAGE_NAME_Release=liblibmuargus.dll.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/liblibmuargus.dll.tar
 #
 # include compiler specific variables
 #
